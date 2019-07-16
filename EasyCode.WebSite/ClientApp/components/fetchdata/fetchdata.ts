e8122167b8d55eac35b5c5fb0c3542e2f2bdf9c6
luckearth <luckearth@luckearth.cn>
2019-07-16 16:45:05 +0800

    简单修改前端界面

--- EasyCode.WebSite/ClientApp/components/fetchdata/fetchdata.ts
+++ EasyCode.WebSite/ClientApp/components/fetchdata/fetchdata.ts
@@ -13,7 +13,13 @@ export default class FetchDataComponent extends Vue {
     forecasts: WeatherForecast[] = [];
 
     mounted() {
-        fetch('api/SampleData/WeatherForecasts')
+        fetch('api/SampleData/WeatherForecasts',
+            {
+                method:"get",
+                    headers: {
+                        "Content-type": "application/json",
+                        "Token": "f4c902c9ae5a2a9d8f84868ad064e706" }
+                })
             .then(response => response.json() as Promise<WeatherForecast[]>)
             .then(data => {
                 this.forecasts = data;
