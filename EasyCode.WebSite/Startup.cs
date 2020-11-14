828be7a915a519fe9e064b88a38a21e13675af50
luckearth <luckearth@luckearth.cn>
2020-11-14 20:36:58 +0800

    升级项目到 .net 5

--- EasyCode.WebSite/Startup.cs
+++ EasyCode.WebSite/Startup.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.AspNetCore.SpaServices;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
